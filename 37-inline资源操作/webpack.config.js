fe36a71724aa3e986a6b88a8c5e6a4b583c92640
王志宇 <wangzhiyu@iimt.org.cn>
2023-12-14 15:10:52 +0800

    webpack指南: 资源资源模块完成inline自定义data URI输出格式的示例编写以及文档编写

--- 37-inline资源操作/webpack.config.js
+++ 37-inline资源操作/webpack.config.js
@@ -2,11 +2,12 @@
  * @Author: wangzhiyu <w19165802736@163.com>
  * @version: 1.0.0
  * @Date: 2023-11-20 09:28:31
- * @LastEditTime: 2023-12-14 14:39:17
+ * @LastEditTime: 2023-12-14 15:08:07
  * @Descripttion: webpack配置文件
  */
 const path = require('path');
 const htmlWebpackPlugin = require('html-webpack-plugin');
+const svgToMiniDataURI = require('mini-svg-data-uri');
 
 module.exports = {
   mode: 'development',
@@ -28,6 +29,14 @@ module.exports = {
         test: /\.svg/,
         // 配置资源模块类型为inline,这样就可以将对应的文件类型转换为base64等其他内联格式
         type: 'asset/inline',
+        // inline默认的输出格式为base64,这里可以通过一个函数来配置输出返回的格式,这里借助了mini-svg-data-uri库,所有返回的格式都会经过mini-svg-data-uri库处理
+        generator: {
+          dataUrl: content => {
+            console.log(content, 'content');
+            content = content.toString();
+            return svgToMiniDataURI(content);
+          },
+        },
       },
     ],
   },
